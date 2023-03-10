97bf1e6373aa766d45240412478352e96bb39238
lalalal99 <francesco.bucci99@hotmail.com>
2023-03-10 12:40:40 +0100

    aggiunto genere mix

--- spotify_top_songs.py
+++ spotify_top_songs.py
@@ -1,4 +1,4 @@
-# from pprint import pprint
+from pprint import pprint
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
@@ -7,25 +7,28 @@ sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 pl_id = {"default": 'https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF',
          "edm": 'https://open.spotify.com/playlist/37i9dQZF1DX3Kdv0IChEm9',
          "metal": 'https://open.spotify.com/playlist/37i9dQZF1EQpgT26jgbgRI',
-         "rock": 'https://open.spotify.com/playlist/37i9dQZF1DWSAm0NxvFu7q'}
+         }
 pl_id["mix"] = list(pl_id.values())
+
+
+def get_songs(query):
+    return sp.playlist_items(query, offset=0, fields='items.track.name,items.track.artists.name', additional_types=['track'])
+
 
 def get_chart(genre):
     response = dict()
     if genre == "mix":
-        for playlist in pl_id["mix"]:
-            response.update(sp.playlist_items(playlist,
-                                 offset=0,
-                                 fields='items.track.name,items.track.artists.name',
-                                 additional_types=['track']))
-    else:  
-        response = sp.playlist_items(pl_id[genre],
-                                    offset=0,
-                                    fields='items.track.name,items.track.artists.name',
-                                    additional_types=['track'])    
+        response = get_songs(pl_id["mix"][0])
+
+        for playlist in pl_id["mix"][1:]:
+            songs = get_songs(playlist)
+            response["items"] += (songs["items"])
+    else:
+        response = get_songs(pl_id[genre])
+
     chart = []
     for item in response["items"]:
-        item = item["track"]
-        chart.append({"Artist": item["name"],
-                      "TrackName": item["artists"][0]["name"]})
+        track = item["track"]
+        chart.append({"Artist": track["name"],
+                      "TrackName": track["artists"][0]["name"]})
     return chart
